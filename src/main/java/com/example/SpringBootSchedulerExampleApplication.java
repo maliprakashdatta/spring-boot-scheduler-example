2e0b4676ba524602bf9853674e8ac39050484bc0
maliprakash092@gmail.com <maliprakash092@gmail.com>
2024-10-05 23:55:48 +0530

    Scheduler

---
+++ src/main/java/com/example/SpringBootSchedulerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSchedulerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSchedulerExampleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/scheduler/config/SchedulerConfig.java
@@ -0,0 +1,29 @@
+package com.example.scheduler.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+
+    @Bean
+    public Executor taskExecutor() {
+        return Executors.newScheduledThreadPool(10);
+    }
+
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        taskRegistrar.setScheduler(taskExecutor());
+    }
+
+    @Bean(name = "poolScheduler")
+    public Executor poolScheduler() {
+        return Executors.newScheduledThreadPool(5);
+    }
+}
